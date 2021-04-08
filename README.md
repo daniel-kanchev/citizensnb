53fc15778834a3d415fa0eaf41ceed4f9a85243e
daniel-kanchev <daniel.kanchev@adata.pro>
2021-04-08 14:56:58 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,13 @@
+URL: https://www.citizensnb.com/news/
+
+    Spider name: citizensnb
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Time: 6.21
+
+Number of articles: 7
---
+++ citizensnb/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'citizensnb'
+SPIDER_MODULES = ['citizensnb.spiders']
+NEWSPIDER_MODULE = 'citizensnb.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'citizensnb.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
